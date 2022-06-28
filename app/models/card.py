283f5b597bc15fb4fef4f1075adca1dcc7e89d20
Thu Vuong <tavuongg@gmail.com>
2022-06-27 20:59:50 -0700

    added Board and Card models

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,6 @@
 from app import db
+
+class Card(db.Model):
+    card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    message = db.Column(db.String, nullable=False)
+    likes_count = db.Column(db.Integer, nullable=True)
