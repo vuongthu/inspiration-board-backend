1bd8c5dd156b2742377bddb110c513550a7fd9e4
Thu Vuong <tavuongg@gmail.com>
2022-06-28 19:45:55 -0700

    fixed foreign key for board_id in card model

--- app/models/card.py
+++ app/models/card.py
@@ -4,7 +4,7 @@ class Card(db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String, nullable=False)
     likes_count = db.Column(db.Integer, nullable=True)
-    board_id = db.Column(db.Integer, db.ForeignKey('card.card_id'), nullable=False)
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=False)
     board = db.relationship("Board", back_populates="cards")
 
     def to_dict(self):
