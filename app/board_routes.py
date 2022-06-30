14a19ffc486a80d20b0e8977c340eb2d7442eaae
Ashley Benson <bensona2@spu.edu>
2022-06-29 20:10:38 -0700

    card get by id endpoint and clean up formatting

--- app/board_routes.py
+++ app/board_routes.py
@@ -7,28 +7,14 @@ from .routes_helper import error_msg, success_msg, make_model, get_model_by_id
 
 bp = Blueprint("boards_bp", __name__, url_prefix="/boards")
 
-#POST/boards
-@bp.route("", methods=["POST"])
-def create_board():
-    request_body = request.get_json()
-
-    new_board = make_model(Board, request_body)
-
-    db.session.add(new_board)
-    db.session.commit()
-
-    return jsonify({"board": new_board.to_dict()}), 201
-
 
 #GET/boards
 @bp.route("", methods=("GET",))
 def get_all_boards():
-
     boards = Board.query.order_by("board_id").all()
-    
     board_list = [board.to_dict() for board in boards]
-
     return jsonify(board_list), 200
+
 
 #GET/ board by id   
 @bp.route("<board_id>", methods=["GET"])
@@ -37,6 +23,16 @@ def get_one_board(board_id):
     return jsonify({"task": task.to_json()}), 200
 
 
+#POST/boards
+@bp.route("", methods=["POST"])
+def create_board():
+    request_body = request.get_json()
+    new_board = make_model(Board, request_body)
+    db.session.add(new_board)
+    db.session.commit()
+    return jsonify({"board": new_board.to_dict()}), 201
+
+#POST/board by id
 @bp.route("/<board_id>/cards", methods=["POST"])
 def create_card(board_id):
     request_body = request.get_json()
--- app/card_routes.py
+++ app/card_routes.py
@@ -7,6 +7,14 @@ from .routes_helper import success_msg, make_model, get_model_by_id
 bp = Blueprint("cards_bp", __name__, url_prefix="/cards")
 
 #GET/card by id
+@bp.route("/<card_id>/cards", methods=["POST"])
+def create_card(card_id):
+    request_body = request.get_json()
+    new_card = make_model(Card, request_body, card_id=card_id)
+    db.session.add(new_card)
+    db.session.commit()
+    return jsonify({"card": new_card.to_dict()}), 201
+
 
 #PATCH/card
 @bp.route("/<card_id>/like", methods=["PATCH"])
