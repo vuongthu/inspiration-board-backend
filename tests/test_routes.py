9954ff57668e09ad728cf8253b49d089ef17b602
Thu Vuong <tavuongg@gmail.com>
2022-07-01 20:32:23 -0700

    reformatted endpoints

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,79 +1,14 @@
-from urllib import response
 import pytest
 from app.models.board import Board
 from app.models.card import Card
 
-
-#POST/boards
-def test_create_board(client):
-    response = client.post("/boards", json={
-        "title": "Pets in the 70s",
-        "owner": "Nostalgia",
-    })
-    response_body = response.get_json()
-
-    assert response.status_code == 201
-    assert "board" in response_body
-    assert response_body == {
-        "board": {
-            "id": 1,
-            "title": "Pets in the 70s",
-            "owner": "Nostalgia",
-        }
-    }
-    new_board = Board.query.get(1)
-    assert new_board
-    assert new_board.title == "Pets in the 70s"
-    assert new_board.owner == "Nostalgia"
-
-
-#POST/boards
-def test_create_card_missing_keys(client):
-    response = client.post("/boards", json={
-        "message": "Enjoy summer",
-        })
-    response_body = response.get_json()
-    assert response.status_code == 400
-    assert response_body == {
-        "details": "Invalid data"
-    }
-
-
-#POST /boards/1/cards
-def test_create_card(client, one_board):
-    response = client.post("/boards/1/cards", json={
-        "message": "Enjoy summer",
-    })    
-    response_body = response.get_json()
-
-    assert response.status_code == 201
-    assert response_body == {
-        "card":{
-        "likes_count": 0,
-        "id": 1,
-        "message": "Enjoy summer"}
-    }
-    new_card = Card.query.get(1)
-    assert new_card
-    assert new_card.message == "Enjoy summer"
-
-
-#POST /boards/1/cards 
-def create_card_missing_message(client, one_board):
-    response = client.post("/boards/1/cards", json={})
-    response_body = response.get_json()
-
-    assert response.status_code == 404
-    assert response_body == {"details": "No Board data with id: 1"}
-
 #GET/boards
-def test_get_no_saved_boards(client):
+def test_get_boards_no_saved_boards(client):
     response = client.get("/boards")
     response_body = response.get_json()
 
     assert response.status_code == 200
     assert response_body == []
-
 
 
 #GET/boards
@@ -82,6 +17,7 @@ def test_get_boards(client, one_board):
     response_body = response.get_json()
 
     assert response.status_code == 200
+    assert len(response_body) == 1
     assert response_body == [{
             "id" : 1,
             "title": "Motivational Quotes",
@@ -104,6 +40,7 @@ def test_get_cards_by_board_id(client, one_card):
     response_body = response.get_json()
 
     assert response.status_code == 200
+    assert len(response_body) == 1
     assert  response_body == [{
             "id" : 1,
             "likes_count" : 0,
@@ -118,8 +55,74 @@ def test_get_cards_when_no_board_id(client):
 
     assert response.status_code == 404
     assert response_body == {
-    "details" : "No Board data with id: 1"   
+    "details" : "No Board data with id: 1"
     }
+
+
+#POST/boards
+def test_create_board(client):
+    response = client.post("/boards", json={
+        "title": "Pets in the 70s",
+        "owner": "Nostalgia",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert "board" in response_body
+    assert response_body == {
+        "board": {
+            "id": 1,
+            "title": "Pets in the 70s",
+            "owner": "Nostalgia",
+        }
+    }
+
+    new_board = Board.query.get(1)
+    
+    assert new_board
+    assert new_board.title == "Pets in the 70s"
+    assert new_board.owner == "Nostalgia"
+
+
+#POST/boards
+def test_create_board_missing_keys(client):
+    response = client.post("/boards", json={})
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"details": "Invalid data"}
+
+
+#POST /boards/1/cards
+def test_create_card(client, one_board):
+    response = client.post("/boards/1/cards", json={
+        "message": "Enjoy summer",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "card": {
+        "likes_count": 0,
+        "id": 1,
+        "message": "Enjoy summer"
+        }
+    }
+    
+    new_card = Card.query.get(1)
+
+    assert new_card
+    assert new_card.message == "Enjoy summer"
+    assert new_card.likes_count == None
+
+
+#POST /boards/1/cards 
+def test_create_card_missing_message(client, one_board):
+    response = client.post("/boards/1/cards", json={})
+    response_body = response.get_json()
+
+    assert response.status_code == 400
+    assert response_body == {"details": "Invalid data"}
 
 
 #DELETE /boards/1
