10755f383638e90da05ca405308ea077d4251918
Jenny C <jennyhc888@gmail.com>
2022-07-01 19:51:44 -0700

    write tests for card and board

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,70 @@ from app.models.board import Board
 from app.models.card import Card
 
 
+#POST/boards
+def test_create_board(client):
+    response = client.post("/boards", json={
+        "title": "Pets in the 70s",
+        "owner": "Nostalgia",
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert "board" in response_body
+    assert response_body == {
+        "board": {
+            "id": 1,
+            "title": "Pets in the 70s",
+            "owner": "Nostalgia",
+        }
+    }
+    new_board = Board.query.get(1)
+    assert new_board
+    assert new_board.title == "Pets in the 70s"
+    assert new_board.owner == "Nostalgia"
+
+
+#POST/boards
+def test_create_card_missing_keys(client):
+    response = client.post("/boards", json={
+        "message": "Enjoy summer",
+        })
+    response_body = response.get_json()
+    assert response.status_code == 400
+    assert response_body == {
+        "details": "Invalid data"
+    }
+
+
+#POST /boards/1/cards
+def test_create_card(client, one_board):
+    response = client.post("/boards/1/cards", json={
+        "message": "Enjoy summer",
+    })    
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "card":{
+        "likes_count": 0,
+        "id": 1,
+        "message": "Enjoy summer"}
+    }
+    new_card = Card.query.get(1)
+    assert new_card
+    assert new_card.message == "Enjoy summer"
+
+
+#POST /boards/1/cards 
+def create_card_missing_message(client, one_board):
+    response = client.post("/boards/1/cards", json={})
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"details": "No Board data with id: 1"}
+
+
+
 #DELETE /boards/1
 def test_delete_board(client, one_board):
     response = client.delete("/boards/1")
